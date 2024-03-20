08ba8a684a1c071123722991d8888b06ea5e4b36
LinCong <3022150480@qq.com>
2024-03-20 15:51:26 +0800

    check ast

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "utils/logger.h"
 #include "IR/context/context.h"
 #include "LAP/parser/parser.hpp"
 
@@ -10,6 +11,10 @@ int main() {
     freopen("../../test/test.cpp", "r", stdin);
     std::unique_ptr<mcs::Node> ast;
     yyparse(ast);
+    if (ast == nullptr) {
+        LOG_ERROR("ast is nullptr.");
+        return 0;
+    }
     ast->codeGen();
     printIR();
     return 0;
